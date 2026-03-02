cec8427677dfa740d67361394ab769926c223864
cookiesmay <fynmwc@gmail.com>
2026-03-02 16:12:06 +1000

    Activity 3 part 2

--- src/User.java
+++ src/User.java
@@ -36,5 +36,25 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-}{
+User.java (click to expand)
+    public class User {
+        // ... (other fields and methods)
+
+        private ArrayList<ToDoItem> toDoItems;
+
+        public User(String username, String password) {
+            // ... (other initializations)
+            this.toDoItems = new ArrayList<>();
+        }
+
+        /**
+         * Gets the to-do items of the user.
+         * @return A reference to the list of to-do items, which can be modified from outside the class.
+         */
+        public ArrayList<ToDoItem> getToDoItems() {
+            return toDoItems;
+        }
+    }
+
 }
+
